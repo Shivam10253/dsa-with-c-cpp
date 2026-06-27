4cd54c67ae3da030b3946563716ab4cd36e10be0
Shivam <148491558+Shivam10253@users.noreply.github.com>
2026-06-27 09:36:17 +0530

    Add array basics example in array_basics.c
    add array declaration & initialization example.

---
+++ Arrays/array_basics.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int A[5]; // declaration
+  int B[5] = {2, 4, 6, 8, 10}; // declaration + initialization
+
+  int i;
+  for (i = 0; i < 5; i++) {
+    printf("%d", B[i];
+  }
+}
