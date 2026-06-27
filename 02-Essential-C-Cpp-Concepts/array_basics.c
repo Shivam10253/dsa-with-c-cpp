97da67a4dfae2f465b8f701e69c855452486bf97
Shivam <148491558+Shivam10253@users.noreply.github.com>
2026-06-27 09:49:05 +0530

    Added array basics program

--- 02-Essential-C-Cpp-Concepts/array_basics.c
+++ 02-Essential-C-Cpp-Concepts/array_basics.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main() {
+    int A[5]; // declaration
+    int B[5] = {2, 4, 6, 8, 10}; // declaration with initialization
+
+    int i;
+    for (i = 0; i < 5; i++) {
+        printf("%d", B[i]);
+    }
+
+}
